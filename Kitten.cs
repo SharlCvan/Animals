27e9f53e484eca1f66e74059a4722bb4c44f0dde
Carl Svahn <calle_tag@hotmail.com>
2019-11-06 10:21:51 +0100

    Create Kitten.cs

---
+++ Kitten.cs
@@ -0,0 +1,21 @@
+﻿namespace Animal
+{
+    class Kitten : Cat, ISound
+    {
+
+        public Kitten(int lives, string name, int age, Gender sex = Gender.Female) : base(lives, name, sex, age)
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return "\nKitten name: " + Name + "\nKitten age: " + Age + "\nKitten gender: " + Sex + "\nKitten number of lives: " + Lives;
+        }
+
+        public string Sound()
+        {
+            return "Mjaooouuu";
+        }
+    }
+}
