9decdde5e383b361f77863046603bc7e93a69ffe
Carl Svahn <calle_tag@hotmail.com>
2019-11-06 10:21:42 +0100

    Create Dog.cs

---
+++ Dog.cs
@@ -0,0 +1,59 @@
+﻿namespace Animal
+{
+    class Dog : Animal, ISound
+    {
+        public enum DogRace
+        {
+            Dalmatin, Pitbull, Pug, GreatDane, Pudel, GoldenRetreaver, Labrador
+        }
+
+        private DogRace race;
+
+        public enum FurColor
+        {
+            Black, Brown, Blonde, White, Spotted, other
+        }
+
+        private FurColor furColor;
+
+        public DogRace Race
+        {
+            get
+            {
+                return race;
+            }
+            set
+            {
+                race = value;
+            }
+        }
+
+        public FurColor Color
+        {
+            get
+            {
+                return furColor;
+            }
+            set
+            {
+                furColor = value;
+            }
+        }
+
+        public Dog(DogRace race, FurColor color, string name, Gender sex, int age) : base(name, sex, age)
+        {
+            Race = race;
+            Color = color;
+        }
+
+        public override string ToString()
+        {
+            return "\nDog name: " + Name + "\nDog Age: " + Age + "\nDog Race: " + Race + "\nDog Gender: " + Sex + "\nDog Color: " + Color;
+        }
+
+        public string Sound()
+        {
+            return "Ruff!!";
+        }
+    }
+}
