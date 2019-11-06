c434497718068d1080cec31bbb41eeb01b99c144
Carl Svahn <calle_tag@hotmail.com>
2019-11-06 10:21:31 +0100

    Create Bobcat.cs

---
+++ Bobcat.cs
@@ -0,0 +1,20 @@
+﻿namespace Animal
+{
+    class Bobcat : Cat, ISound
+    {
+        public Bobcat(int lives, string name, int age, Gender sex = Gender.Male) : base(lives, name, sex, age)
+        {
+
+        }
+
+        public string Sound()
+        {
+            return "Raaaawwwrrrr!!!";
+        }
+
+        public override string ToString()
+        {
+            return "\nBobcat name: " + Name + "\nBobcat age: " + Age + "\nBobcat gender: " + Sex + "\nBobcat number of lives: " + Lives;
+        }
+    }
+}
