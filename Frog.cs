459b9c205303589bc5aa93b942ca1831b9b360da
Carl Svahn <calle_tag@hotmail.com>
2019-11-06 10:21:45 +0100

    Create Frog.cs

---
+++ Frog.cs
@@ -0,0 +1,64 @@
+﻿namespace Animal
+{
+    class Frog : Animal, ISound
+    {
+        public enum FrogColor
+        {
+            Green, Red, Black, White, Orange, Pink
+        }
+
+        private FrogColor color;
+        private bool poisonus;
+
+        public FrogColor Color
+        {
+            get
+            {
+                return color;
+            }
+            set
+            {
+                color = value;
+            }
+        }
+
+        public bool Poisonus
+        {
+            get
+            {
+                return poisonus;
+            }
+            set
+            {
+                poisonus = value;
+            }
+        }
+
+        public Frog(FrogColor color, bool poisonus, string name, Gender sex, int age) : base(name, sex, age)
+        {
+            Color = color;
+            Poisonus = poisonus;
+        }
+
+        public override string ToString()
+        {
+            string result = "";
+
+            if (poisonus)
+            {
+                result = "Yes";
+            }
+            else
+            {
+                result = "No";
+            }
+
+            return "Frog name: " + Name + "\nFrog Age: " + Age + "\nFrog Gender: " + Sex + "\nFrog poisonus: " + result + "\nFrog Color: " + Color;
+        }
+
+        public string Sound()
+        {
+            return "Ribbit, Ribbit";
+        }
+    }
+}
