a648c57fc17775cad980f4b822604e9e4fb3d3a3
Carl Svahn <calle_tag@hotmail.com>
2019-11-06 10:21:28 +0100

    Create Animal.cs

---
+++ Animal.cs
@@ -0,0 +1,60 @@
+﻿namespace Animal
+{
+    class Animal
+    {
+        private int age;
+        private string name;
+
+        public enum Gender
+        {
+            Male, Female
+        }
+
+        private Gender sex;
+
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                age = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public Gender Sex
+        {
+            get
+            {
+                return sex;
+            }
+            set
+            {
+                sex = value;
+            }
+        }
+
+        public Animal(string name, Gender sex, int age)
+        {
+            Name = name;
+            Sex = sex;
+            Age = age;
+        }
+
+
+    }
+}
