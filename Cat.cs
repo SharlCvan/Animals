a29f5d7feb63a49cd2018e089b55477f895f38ad
Carl Svahn <calle_tag@hotmail.com>
2019-11-06 10:21:39 +0100

    Create Cat.cs

---
+++ Cat.cs
@@ -0,0 +1,25 @@
+﻿namespace Animal
+{
+    class Cat : Animal
+    {
+        private int lives;
+
+        public int Lives
+        {
+            get
+            {
+                return lives;
+            }
+            set
+            {
+                lives = value;
+            }
+        }
+
+        public Cat(int lives, string name, Gender sex, int age) : base(name, sex, age)
+        {
+            Lives = lives;
+        }
+
+    }
+}
